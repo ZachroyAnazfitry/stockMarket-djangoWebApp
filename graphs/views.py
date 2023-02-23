eedbbf01f1861e1ca222e9550153c6e2cadd2860
ZachAnaz <zachroy04@yahoo.com>
2023-02-23 12:23:31 +0800

    search bar - api iexcloud

--- graphs/views.py
+++ graphs/views.py
@@ -5,6 +5,7 @@ def home(request):
 
     import requests
     import json
+    import urllib.parse
 
      # pk_d6bfc8b1acda47ea906bdb7e6fe9ae4b
 
@@ -12,19 +13,39 @@ def home(request):
     api_request = requests.get("https://api.iex.cloud/v1/data/core/quote/aapl?token=pk_d6bfc8b1acda47ea906bdb7e6fe9ae4b")
 
     # error handling to test whether connection success or not
+    # try:
+    #     api = json.loads(api_request.content)
 
-    try:
-        api = json.loads(api_request.content)
+      
+    # except Exception as e:
+    #     api = "This is not getting data"
+
+    # for search bar
+    if request.method == 'POST':
+        # ticker = ["AAPL", "AMZN","TSLA"] # replace with your ticker symbol
+        ticker = request.POST['ticker']
+        # encoded_ticker = urllib.parse.quote(ticker)
+
+        api_request = requests.get(f"https://api.iex.cloud/v1/data/core/quote/{ticker}?token=pk_d6bfc8b1acda47ea906bdb7e6fe9ae4b")
+
+        try:
+            api = json.loads(api_request.content)
 
         # api = {}
         # for i in api:
         #     api.update(i)
-    except Exception as e:
-        api = "This is not getting data"
+        except Exception as e:
+            api = "This is not getting data"
+        
+        return render(request, 'home.html', {'api':api})
+
+    else:
+        return render(request, 'home.html', {'ticker': "Enter a ticker symbol"})
+
 
     
 
-    return render(request, 'home.html', {'api':api})
+
 
 
 # dipslay view for about
